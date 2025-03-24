69c0c092464df1b31972aa96f11dac0f88da16eb
Afsal T A <afsal@finanshels.com>
2025-03-24 17:50:49 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import sys, os
 
-st.set_page_config(page_title="🤖 Integrated Bot", layout="centered")
+st.set_page_config(page_title="Integrated Bot", layout="centered")
 
 # Import the apps
 sys.path.append(os.path.abspath("pdf_app"))
@@ -32,7 +32,7 @@ st.markdown("""
     </style>
 """, unsafe_allow_html=True)
 
-st.markdown("<div class='title'>🤖 Integrated Bot</div>", unsafe_allow_html=True)
+st.markdown("🤖"+<div class='title'>Integrated Bot</div>", unsafe_allow_html=True)
 
 # Tab switch
 default_tab = "📄 PDF to CSV Converter"
