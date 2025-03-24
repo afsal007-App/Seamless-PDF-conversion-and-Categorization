77907b11178b93a641b7541d90cb9960e2aa281e
Afsal T A <afsal@finanshels.com>
2025-03-24 17:34:33 +0530

    Update main.py

--- main.py
+++ main.py
@@ -22,6 +22,13 @@ st.markdown("""
         -webkit-background-clip: text;
         -webkit-text-fill-color: transparent;
     }
+     @keyframes glow {
+        from {
+            text-shadow: 0 0 05px ##886dc7, 0 0 10px ##cdb4d4;
+        }
+        to {
+            text-shadow: 0 0 12px #EBE3D5, 0 0 2px #B5CB99;
+        }
     </style>
 """, unsafe_allow_html=True)
 
