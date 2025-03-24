247daeeb7ecfba0c1f1fdf5f743845bfb2235e92
Afsal T A <afsal@finanshels.com>
2025-03-25 01:02:35 +0530

    Update Rak_Bank.py

--- pdf_app/Rak_Bank.py
+++ pdf_app/Rak_Bank.py
@@ -102,13 +102,12 @@ def run():
 
     all_transactions = []
     final_df = None
-    
+
     if uploaded_files:
-        uploaded_files = sorted(uploaded_files, key=lambda x: x.name.lower())
         for uploaded_file in uploaded_files:
-        st.info(f"Processing: {uploaded_file.name}")
-        transactions = process_pdf(uploaded_file, uploaded_file.name)
-        all_transactions.extend(transactions)
+            st.info(f"Processing: {uploaded_file.name}")
+            transactions = process_pdf(uploaded_file, uploaded_file.name)
+            all_transactions.extend(transactions)
 
         if all_transactions:
             df = pd.DataFrame(all_transactions)
