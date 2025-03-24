a761ea585acdce24cecbf6772980f402d0c436fd
Afsal T A <afsal@finanshels.com>
2025-03-24 22:45:01 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import sys, os
 
-st.set_page_config(page_title="Integrated Bot", layout="centered")
+st.set_page_config(page_title="Integrated Bot", layout="wide")
 
 # Import the apps
 sys.path.append(os.path.abspath("pdf_app"))
