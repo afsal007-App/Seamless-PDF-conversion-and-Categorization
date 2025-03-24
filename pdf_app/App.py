a6fc7a1d10c7d8201bc16c9c11c4fea3edb25099
Afsal T A <afsal@finanshels.com>
2025-03-24 22:54:21 +0530

    Update App.py

--- pdf_app/App.py
+++ pdf_app/App.py
@@ -77,8 +77,11 @@ def run():
         }
 
         @keyframes glow {
-            from { text-shadow: 0 0 5px #886dc7, 0 0 10px #cdb4d4; }
-            to { text-shadow: 0 0 12px #EBE3D5, 0 0 2px #B5CB99; }
+            from { 
+                 text-shadow: 0 0 5px #886dc7, 0 0 10px #cdb4d4; 
+                 }
+            to { text-shadow: 0 0 12px #EBE3D5, 0 0 2px #B5CB99; 
+            }
         }
         </style>
     """, unsafe_allow_html=True)
