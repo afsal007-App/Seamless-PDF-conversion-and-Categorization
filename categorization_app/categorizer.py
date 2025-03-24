a8afbee22aa2a80530ee8efc9a33fd7808e6fc42
Afsal T A <afsal@finanshels.com>
2025-03-24 23:08:39 +0530

    Update categorizer.py

--- categorization_app/categorizer.py
+++ categorization_app/categorizer.py
@@ -29,7 +29,7 @@ def run():
             background: linear-gradient(90deg, #00dbde, #fc00ff);
             -webkit-background-clip: text;
             -webkit-text-fill-color: transparent;
-            animation: glow 2s ease-in-out infinite alternate;
+           
         }
 
         .watermark {
@@ -84,14 +84,7 @@ def run():
             padding-right: 2rem;
         }
 
-        @keyframes glow {
-        from {
-            text-shadow: 0 0 5px #9d4edd, 0 0 10px #c77dff;
-        }
-        to {
-            text-shadow: 0 0 12px #e0aaff, 0 0 4px #b5cb99;
-        }
-        }
+        
 
         @keyframes fadeIn {
             from { opacity: 0; }
