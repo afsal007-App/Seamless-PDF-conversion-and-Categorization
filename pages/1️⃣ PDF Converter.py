6156c9ea9e03963492a827b2afe885d400548dde
Afsal T A <afsal@finanshels.com>
2025-03-24 13:57:08 +0530

    Delete pages directory

--- pages/1️⃣ PDF Converter.py
+++
@@ -1,5 +0,0 @@
-import sys, os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "pdf_app")))
-import app as pdf_app
-
-pdf_app.run()
--- pages/2️⃣ Categorizer.py
+++
@@ -1,5 +0,0 @@
-import sys, os
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "categorization_app")))
-import app as categorizer_app
-
-categorizer_app.run()
